72dac308a737ac4b7c9f14ed182a8ae8e0e0d4c4
aratar79 <aratar79@gmail.com>
2021-07-14 22:21:44 +0200

    implement service layer

--- src/BlazorPokedex.Client/Program.cs
+++ src/BlazorPokedex.Client/Program.cs
@@ -1,3 +1,4 @@
+using BlazorPokedex.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,7 +18,8 @@ namespace BlazorPokedex.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://pokeapi.co/api/v2/") });
+            builder.Services.AddScoped<IPokeApiClient, PokeApiClient>();
 
             await builder.Build().RunAsync();
         }
---
+++ src/BlazorPokedex.Services/PokeApiClient.cs
@@ -0,0 +1,34 @@
+﻿using BlazorPokedex.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPokedex.Services
+{
+    public class PokeApiClient : IPokeApiClient
+    {
+        private readonly HttpClient _httpClient;
+        public PokeApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IEnumerable<Pokemon>> GetAllPokemons()
+        {
+            var result = new List<Pokemon>();
+
+            var request = JsonConvert.DeserializeObject<ResultObject>(await _httpClient.GetStringAsync("pokemon?limit=24&offset=24"));
+            foreach (var pok in request)
+            {
+
+            }
+        }
+
+        public async Task<Pokemon> GetPokemon(string name) =>
+            JsonConvert.DeserializeObject<Pokemon>(await _httpClient.GetStringAsync($"pokemon/{name}"));
+
+    }
+}
